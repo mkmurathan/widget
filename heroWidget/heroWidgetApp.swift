e7cf5b23addde3c6a92f2bd7f31b8dccb809532a
Murathan Karagöz <murathankaragooz@hotmail.com>
2023-08-24 22:06:04 +0300

    Initial Commit

---
+++ heroWidget/heroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  heroWidgetApp.swift
+//  heroWidget
+//
+//  Created by Murathan karagöz on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct heroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
